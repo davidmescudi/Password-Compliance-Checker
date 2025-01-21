24c9ded831354da6a6dafa59b45d04c4531fec56
davidmescudi <social@davidabbe.de>
2025-01-21 18:57:29 +0100

    Enhance logging in background and content scripts for better debugging

--- background.js
+++ background.js
@@ -1,4 +1,8 @@
+const initiator = 'background.js';
+console.debug(initiator, 'Background script loaded');
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log('Message received:', request);
   if (request.password) {
     console.warn('Password detected:', request.password);
     // TODO: Send the password to the LLM for validation
@@ -12,10 +16,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     .then(response => response.json())
     .then(data => {
       // Is this needed?
-      console.log('Password validation result:', data);
+      console.log('Result:', data);
     })
     .catch(error => {
-      console.error('Error validating password:', error);
+      console.error('Error:', error);
     });*/
   }
 });
--- content.js
+++ content.js
@@ -1,5 +1,10 @@
+const initiator = 'content.js';
+console.debug(initiator, 'Content script loaded');
+
 document.addEventListener('input', function(event) {
+  // TODO: only send when the input field looses focus or the user presses enter
   if (event.target.type === 'password') {
+    console.debug('Password input detected');
     chrome.runtime.sendMessage({ password: event.target.value });
   }
 });
